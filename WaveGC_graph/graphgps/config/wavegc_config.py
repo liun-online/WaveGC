336430214ecb7a7f6b804bfce536a6b6b947aa18
Nian Liu <52119585+liun-online@users.noreply.github.com>
2024-02-23 12:33:36 +0800

    Add files via upload

---
+++ WaveGC_graph/graphgps/config/wavegc_config.py
@@ -0,0 +1,17 @@
+from torch_geometric.graphgym.register import register_config
+from yacs.config import CfgNode as CN
+
+
+@register_config('cfg_WaveGC')
+def set_cfg_lam(cfg):
+    cfg.WaveGC = CN()
+    cfg.WaveGC.nheads = 8
+    cfg.WaveGC.trans_dropout = 0.1
+    cfg.WaveGC.adj_dropout = 0.0
+    cfg.WaveGC.drop = 0.5
+    cfg.WaveGC.num_n = 5
+    cfg.WaveGC.num_J = 3
+    cfg.WaveGC.pre_s = [10.0, 10.0, 10.0]
+    cfg.WaveGC.tight_frames = True
+    cfg.WaveGC.weight_share = False
+    cfg.WaveGC.trans_use = False
